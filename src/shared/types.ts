1d3d1c64deb208bb58d39e8e3fe1b9ffbf5c6b15
gananajak-lgtm <gananajak@gmail.com>
2026-09-22 11:22:28 +0700

    feat: type local test asset generation

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -319,6 +319,7 @@ export type DesktopApi = {
   saveElevenLabsApiKey: (apiKey: string) => Promise<ContentProviderStatus>;
   generateContentProject: (brief: ContentBrief) => Promise<ContentProject>;
   generateLocalTestProject: (brief: ContentBrief) => Promise<ContentProject>;
+  generateLocalTestAssets: (project: ContentProject) => Promise<ContentProject>;
   prepareContentBatch: (briefs: ContentBrief[]) => Promise<ContentBatch>;
   loadContentBatch: () => Promise<ContentBatch | null>;
   resumeContentBatch: (batch: ContentBatch) => Promise<ContentBatch>;
