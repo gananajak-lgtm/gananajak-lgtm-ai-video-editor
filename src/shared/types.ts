c2de7481a5ca0167e1ca3a35d7efd462378ad9dd
gananajak-lgtm <gananajak@gmail.com>
2026-09-25 21:59:14 +0700

    feat: type YouTube OAuth configuration bridge

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -326,6 +326,7 @@ export type DesktopApi = {
   loadContentBatch: () => Promise<ContentBatch | null>;
   updatePublishPlan: (batch: ContentBatch, itemId: string, publish: import("./content-factory").PublishPlan) => Promise<ContentBatch>;
   getPublishAccounts: () => Promise<import("./content-factory").PublishAccount[]>;
+  configureYouTubeOAuth: (clientId: string, clientSecret?: string) => Promise<import("./content-factory").PublishAccount[]>;
   resumeContentBatch: (batch: ContentBatch) => Promise<ContentBatch>;
   generateContentBatchAssets: (batch: ContentBatch) => Promise<ContentBatch>;
   chooseBatchOutputFolder: () => Promise<string | null>;
