e540620aa202ecdc3f2c6b0789050ac10255a198
gananajak-lgtm <gananajak@gmail.com>
2026-09-26 00:30:00 +0700

    feat: expose affiliate batch preparation

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -53,6 +53,7 @@ const api: DesktopApi = {
   connectYouTube: () => ipcRenderer.invoke("content:connect-youtube"),
   importAffiliateProduct: (sourceUrl) => ipcRenderer.invoke("affiliate:import-product", sourceUrl),
   createAffiliateJobs: (products) => ipcRenderer.invoke("affiliate:create-jobs", products),
+  prepareAffiliateBatch: (jobs, language, duration) => ipcRenderer.invoke("affiliate:prepare-batch", jobs, language, duration),
   resumeContentBatch: (batch) =>
     ipcRenderer.invoke("content:resume-batch", batch),
   generateContentBatchAssets: (batch) =>
